fcbd54741b638d7bba6dc58aa2e1fa6f38207575
AquilesBailo <hugoisraelgavilima@gmail.com>
2023-05-08 16:02:07 -0500

    noni

---
+++ funciones.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_excel("base_latina.xlsx")
+df["CedulaPasaporte"] = df["CedulaPasaporte"].astype(str)
+df["Beca_Estimada"] = df["Beca_Estimada"].apply(lambda x: str(np.round(x*100, 2)) + " %")
+df["upper.beca"] = df["upper.beca"].apply(lambda x: str(np.round(x*100, 2)) + " %")
+
+df = df.rename(columns={"NOMBRE_CARRERA": "Carrera", 
+                       "NOMBRE_ENFASIS": "Énfasis",
+                       "NOMBRE_GRADO": "Grado",
+                       "TOTAL_CURSOS": "Curso",
+                       "Beca_Estimada": "Beca Media",
+                       "upper.beca": "Máximo de Beca"})
+
+
+def buscar(cedula):
+    lts = ["Carrera", "Énfasis", "Grado", "Curso", "Beca Media", "Máximo de Beca"]
+    
+    ff = df.loc[(df["CedulaPasaporte"] == cedula), lts]   
+    
+    
+    
+    return ff
--- run.py
+++ run.py
@@ -1,16 +1,26 @@
 import streamlit as st
+import pandas as pd
+from funciones import*
 
 st.set_page_config(layout="wide", page_title="Cálculo Porcentaje de Beca")
-
-
 st.write("# Cálculo Porcentaje de Beca")
 st.write(
     "La aplicación permite consultar el porcentaje de beca de cada estudiante."
     )
 
 
-a, b, c  = st.columns(3)
+a, b, c, d, f  = st.columns(5)
 
-with b:
-    st.text_input("Favor ingrese la cédula del estudiante.")
+with c:
+    cedula = st.text_input("Favor ingrese la cédula del estudiante.")
+    tabla = buscar(cedula)
+    
+    
+st.write(tabla)
+    
+
+
+    
+    
+
 
