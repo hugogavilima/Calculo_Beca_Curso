d8804919aaadf7443e70372330d69f1e8f28ef4b
AquilesBailo <hugoisraelgavilima@gmail.com>
2023-05-08 12:04:46 -0500

    Inicio de la aplicación.

---
+++ run.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="Cálculo Porcentaje de Beca")
+
+
+st.write("# Cálculo Porcentaje de Beca")
+st.write(
+    "La aplicación permite consultar el porcentaje de beca de cada estudiante."
+    )
+
+st.text_input("Favor ingrese la cédula del estudiante.")
+
